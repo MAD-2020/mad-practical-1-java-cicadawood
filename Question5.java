d91edbac570a298aee16f4a95f490eeb5a1b4b03
cicadawood <48861968+cicadawood@users.noreply.github.com>
2020-04-27 23:45:26 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -26,7 +26,35 @@ public class Question5
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Enter the length of integer set that you are going to enter: ");
+    int lengthOfArray = reader.nextInt();
+  
+    int[]intSet = new int[lengthOfArray];
+
+    int modeValue = 0; int maxCount = 0;
+
+    for(int i = 0; i<lengthOfArray; i++){
+      System.out.print("Enter the integer: ");
+      int integer = reader.nextInt();
+      intSet[i]=integer;
+    }
+
+    for(int i = 0; i<lengthOfArray;i++){
+      int count = 0;
+      for (int j = 0; j<lengthOfArray;j++){
+        if (intSet[j] == intSet[i]){
+          count++;
+        }
+      }
+      if(count>maxCount){
+        maxCount = count;
+        modeValue = intSet[i];
+      }
+    }
+    
+    System.out.print("The mode is "+modeValue); 
+
     
   }
 }
