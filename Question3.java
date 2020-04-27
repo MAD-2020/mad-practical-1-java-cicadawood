2674ee3f26655e2ee0e1e2b326a681cf5590e246
cicadawood <48861968+cicadawood@users.noreply.github.com>
2020-04-27 23:43:54 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -10,7 +10,16 @@ public class Question3
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
+    Scanner reader = new Scanner(System.in);
+    System.out.println("Enter an integer: ");
+    
+    int integer = reader.nextInt();
+    
+    int result = integer * integer;
+   
+    
+    System.out.println("The result is " + result);
+
     
   }
 }
